4d02ae46f5619c5c0e86e0faf7c1c1aac9390c56
Dumble <andrewalvarez919@gmail.com>
2025-10-23 10:39:19 -0400

    changes made to grocery_item.dart

--- pw1/lib/models/grocery_item.dart
+++ pw1/lib/models/grocery_item.dart
@@ -1,46 +1,45 @@
-class GroceryItem{ 
-    final int? id;
-    final String name;
-    final String category;
-    final int quantity;
-    final double price;
-    final String description;
-    final String image;
-    final bool purchased;
+class GroceryItem {
+  final int? id;
+  final String name;
+  final String category;
+  final int quantity;
+  final double price;
+  final String description;
+  final String image;
+  final bool purchased;
 
-    GroceryItem({
-        this.id;
-        require this.name;
-        require this.category,
-        this.quantity = 1;
-        require this.price,
-        this.purchased = false,
-        require this.description,
-        require this.image,
-    });
+  GroceryItem({
+    this.id,
+    required this.name,
+    required this.category,
+    this.quantity = 1,
+    required this.price,
+    required this.description,
+    required this.image,
+    this.purchased = false,
+  });
 
-    // in order to save our items values we use a map or dictionary 
-    Map<String, dynamic> toMap() => {
-        'id' : id,
-        'name' : name,
-        'category' : category,
-        'quantity' : quantity, 
-        'purchased' : purchased,
-        'price' : price, 
-        'description' : description,
-        'image' : image,
-        'purchased' : purchased ? 1 : 0, 
-    };
+  // Convert to map for saving to database
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'name': name,
+        'category': category,
+        'quantity': quantity,
+        'price': price,
+        'description': description,
+        'image': image,
+        'purchased': purchased ? 1 : 0, // SQLite stores bools as 0/1
+      };
 
-    //to covert the map back to an object to load 
-    factory GroceryItem.fromMap(Map<String, dynamic> Map) => GroceryItem(
-        id : map["id"],
-        price : map["price"],
-        description : map["description"]
-        name : map["name"],
-        category : category["category"],
-        quantity : quantity["quantity"],
-        image : quantity["image"],
-        purchased : purchased["purchased"] == 1, //probably make this the check box value. 
-    );
+  // Convert map from database to GroceryItem object
+  factory GroceryItem.fromMap(Map<String, dynamic> map) => GroceryItem(
+        id: map['id'],
+        name: map['name'],
+        category: map['category'],
+        quantity: map['quantity'],
+        price: map['price'],
+        description: map['description'],
+        image: map['image'],
+        purchased: map['purchased'] == 1,
+      );
 }
