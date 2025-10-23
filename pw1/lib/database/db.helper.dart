dce9dc87642e158bd766cbf757554c54555c1fea
Dumble <andrewalvarez919@gmail.com>
2025-10-23 10:37:54 -0400

    changes to the db.helper file

--- pw1/lib/database/db.helper.dart
+++ pw1/lib/database/db.helper.dart
@@ -28,11 +28,126 @@ class DBHelper {
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         category TEXT,
+        price REAL,
+        description TEXT,
         quantity INTEGER,
-        purchased BOOL
+        purchased BOOL,
+        image TEXT
       )
     ''');
   }
+
+  await _initialdata(db)
+}
+
+Future<void> _initialdata(Database db) async{
+    final sample = [
+        {
+        'name': 'Banana',
+        'category': 'Fruits',
+        'quantity': 6,
+        'price': 2.99,
+        'description': 'Banana is high in potassium, and a great source of fiber.',
+        'image': 'assets/images/bananas.png',
+        'purchased': 0,
+        },
+        {
+        'name': 'Yogurt',
+        'category': 'Dairy',
+        'quantity': 4,
+        'price': 1.99,
+        'description': 'Creamy, rich, smooth, great source of calcium, helps with digestion.',
+        'image': 'assets/images/yogurt.png',
+        'purchased': 0,
+        },
+        {
+        'name': 'Bread',
+        'category': 'Bakery',
+        'quantity': 1,
+        'price': 2.49,
+        'description': 'Whole wheat sandwich bread loaf.',
+        'image': 'assets/images/bread.png',
+        'purchased': 0,
+        }
+        {
+        'name': 'Bread',
+        'category': 'Bakery',
+        'quantity': 1,
+        'price': 4.99,
+        'description': 'Whole oat bread, good for deli sandwhiches, and pb & js.',
+        'image': 'assets/images/bread.png',
+        'purchased': 0,
+        }
+        {
+        'name': 'Eggs',
+        'category': 'Poultry',
+        'quantity': 12,
+        'price': 4.99,
+        'description': 'One dozen large free-range eggs, organic.',
+        'image': 'assets/images/eggs.png',
+        'purchased': 0,
+        }
+        {
+        'name': 'Chicken Breast',
+        'category': 'Meat',
+        'quantity': 4,
+        'price': 10.99,
+        'description': 'Boneless skinless chicken breasts (1 lb).',
+        'image': 'assets/images/chicken.png',
+        'purchased': 0,
+        }
+        {
+        'name': 'Rice',
+        'category': 'Grain',
+        'quantity': 1,
+        'price': 4.49,
+        'description': 'Long grain white rice, 5 lb bag.',
+        'image': 'assets/images/rice.png',
+        'purchased': 0,
+        }
+        {
+        'name': 'Apple Cider',
+        'category': 'Beverages',
+        'quantity': 1,
+        'price': 3.59,
+        'description': 'Freshly squeezed apple cider, gallon.',
+        'image': 'assets/images/applecider.png',
+        'purchased': 0,
+        }
+        {
+        'name': 'Brocolli Crowns',
+        'category': 'Vegetable',
+        'quantity': 2,
+        'price': 1.99,
+        'description': 'Great green, high in sulfur, and great with blue cheese.',
+        'image': 'assets/images/brocolli.png',
+        'purchased': 0,
+        }
+        {
+        'name': 'Alfredo Sauce',
+        'category': 'Pantry',
+        'quantity': 1,
+        'price': 2.29,
+        'description': 'Classic Italian-style Alfredo, 24 oz jar.',
+        'image': 'assets/images/sauce.png',
+        'purchased': 0,
+        }
+        {
+        'name': 'Clorox Bleach',
+        'category': 'Cleaning_Supplies',
+        'quantity': 1,
+        'price': 10.99,
+        'description': 'Bottle of foam bleach, good for removing mold, bacteria.',
+        'image': 'assets/images/clorox.png',
+        'purchased': 0,
+        }
+
+    ]
+}
+
+for (var in in sample){
+    await db.insert('groceries', item.toMap());
+}
 
   Future<int> insertItem(GroceryItem item) async {
     final db = await database;
--- pw1/lib/models/grocery_item.dart
+++ pw1/lib/models/grocery_item.dart
@@ -1,29 +1,46 @@
-class { 
+class GroceryItem{ 
+    final int? id;
     final String name;
     final String category;
     final int quantity;
-    bool purchased;
+    final double price;
+    final String description;
+    final String image;
+    final bool purchased;
 
     GroceryItem({
+        this.id;
         require this.name;
         require this.category,
         this.quantity = 1;
+        require this.price,
         this.purchased = false,
+        require this.description,
+        require this.image,
     });
 
     // in order to save our items values we use a map or dictionary 
     Map<String, dynamic> toMap() => {
+        'id' : id,
         'name' : name,
         'category' : category,
         'quantity' : quantity, 
         'purchased' : purchased,
+        'price' : price, 
+        'description' : description,
+        'image' : image,
+        'purchased' : purchased ? 1 : 0, 
     };
 
     //to covert the map back to an object to load 
     factory GroceryItem.fromMap(Map<String, dynamic> Map) => GroceryItem(
+        id : map["id"],
+        price : map["price"],
+        description : map["description"]
         name : map["name"],
         category : category["category"],
         quantity : quantity["quantity"],
-        purchased : purchased["purchased"], //probably make this the check box value. 
+        image : quantity["image"],
+        purchased : purchased["purchased"] == 1, //probably make this the check box value. 
     );
 }
